c990d523c4e25f297111e01601743f0ffe657c1d
Gleb Mezhanskiy <gleb.mezhanskiy@autodesk.com>
2016-02-28 01:10:51 -0800

    adding vols works

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, jsonify, session, redirect
 from app import app, db, models
 import datetime
+import json
+import pdb
 import twilio.twiml
 
 
@@ -17,21 +19,38 @@ def internal_error(error):
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
+@app.route('/register', methods=['GET'])
+@app.route('/login', methods=['GET'])
 def index():
-    return render_template('volunteer.html')
+    return render_template('vol_sign_up.html')
 
-@app.route('/ngo', methods=['GET'])
-def ngo():
-    return render_template('ngo.html')
 
 @app.route('/volunteer/new', methods=['POST'])
 def new_vol():
     data = request.get_json()
     app.logger.info(data)
-    # vol = models.Vol()
-    # db.session.add(vol)
-    # db.session.commit()
-    return str(data)
+    vol = models.Vol(
+        email=data['email'],
+        phone=data['phone'],
+        first_name=data['first_name'],
+        last_name=data['last_name'],
+        linkedin=data['linkedin'],
+        facebook=data['facebook'],
+        occupation=data['occupation'],
+        accepts_texts=(True if data['accepts_texts'].lower() == 'yes' else False),
+        postal_code=data['postal_code'],
+        created_on=datetime.datetime.utcnow(),
+        mon=(True if 'Monday' in data else False),
+        tue=(True if 'Tuesday' in data else False),
+        wed=(True if 'Wednesday' in data else False),
+        thu=(True if 'Thursday' in data else False),
+        fri=(True if 'Friday' in data else False),
+        sat=(True if 'Saturday' in data else False),
+        sun=(True if 'Sunday' in data else False))
+
+    db.session.add(vol)
+    db.session.commit()
+    return redirect('/volunteers')
 
 @app.route('/project/new', methods=['GET','POST'])
 def new_project():
@@ -49,14 +68,16 @@ def project(project_id):
         return render_template('project_info.html')
 
 
-
-
-
-
-@app.route('/volunteer/', methods=['GET'])
-def list_vols():
+@app.route('/project/<int:project_id>/volunteers', methods=['POST'])
+@app.route('/volunteers', methods=['POST'])
+def get_vols_list():
     vols = models.Vol.query.all()
-    return jsonify([vol.__dict__ for vol in vols])
+    jvols = jsonify({'vols':[vol.as_dict() for vol in vols]})
+    return jvols
+
+@app.route('/volunteers', methods=['GET'])
+def list_vols():
+    return render_template('vols_list.html')
 
 
 @app.route('/request_vols', methods=['POST'])
