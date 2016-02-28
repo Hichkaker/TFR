5d8c94271d5c2cfb309cdcde21123ed84fb7db88
Gleb Mezhanskiy <gleb.mezhanskiy@autodesk.com>
2016-02-28 13:06:45 -0800

    fix

--- app/views.py
+++ app/views.py
@@ -92,10 +92,6 @@ def project_data(project_id):
     p = models.Project.query.get(project_id)
     return str(p)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> d0d075c3f573264b927e1c2b53992288ff261e4f
 @app.route('/volunteer', methods=['GET'])
 
 
